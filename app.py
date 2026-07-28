269d6d5d50c414204959300bf9349aed3f2fcb93
Ecesu Güngör <jayesu170524@users.noreply.huggingface.co>
2026-07-28 14:31:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
+from sentence_transformers import SentenceTransformer
+import torch
+
+with open("water_cycle.txt", "r", encoding="utf-8") as file:
+  # Read the entire contents of the file and store it in a variable "r" shows that we're gonna open this in read mode
+  water_cycle_text = file.read()
+
+# Print the text below
+print(water_cycle_text)
 
 # This is the same pattern from the Generative AI lesson! It uses the
 # Inference Provider API to send your messages to an AI model and get
