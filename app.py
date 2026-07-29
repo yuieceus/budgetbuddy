7fc226ee0fda14c41c3f854a56b4d802818c4a47
Eshita Agarwal <Eshita18@users.noreply.huggingface.co>
2026-07-29 14:09:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,19 @@ def respond(message, history):
     )
     return response.choices[0].message.content.strip()
 
-chatbot = gr.ChatInterface(respond)
+
+with gr.Blocks() as chatbot:
+    gr.Image("BudgetBuddy_Image_Header.png", show_label=False)
+    
+    gr.ChatInterface(respond, type = "messages",
+    title = "Hi, I'm BudgetBuddy! 💵",
+    textbox= gr.Textbox(placeholder="Share Your Budget Or Ask Me Anything!"),
+    description = "A smart chatbot that combines budgeting and mental wellness to help you spend mindfully, save better, and stress less!",
+    examples = ["I get $500 per month, can you make me a budget?", 
+                "I keep buying things when I'm stressed",
+                "Help me save for a new phone"]
+                          )
+
 
 chatbot.launch()
 
